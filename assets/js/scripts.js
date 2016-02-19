55cb93ea42014744d5785e399efce704757fb794
Jeremy Smith <smithjeremym@gmail.com>
2016-02-19 08:33:37 -0500

    simplify html structure, get cards to flip and flip back with jquery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,11 @@
-$(function () {
+$(document).ready( function () {
   "use strict";
 
+  //when .card is clicked, do the thing
 
-
-
-
+  $(".card").on("click", function () {
+    $(this).toggleClass("cardFlip");
+  });
 
 
 
